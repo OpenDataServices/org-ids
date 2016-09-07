540611336929b52f5a93b76b26b9778fa3868f8d
David Raznick <kindly@gmail.com>
2016-09-07 11:05:18 +0100

    Add about, and format results using titles.

--- prefix_finder/frontend/templatetags/results.py
+++ prefix_finder/frontend/templatetags/results.py
@@ -3,26 +3,61 @@ from django import template
 register = template.Library()
 
 SKIP_KEYS = '''name url code structure
-               weight jurisdiction_flat structure_flat'''.split()
+               jurisdiction_flat structure_flat'''.split()
+
+name_to_identifier = {"code": "code",
+                      "name": "name",
+                      "Description": "description",
+                      "Jurisdiction": "jurisdiction",
+                      "url": "url",
+                      "Public Database": "public-database",
+                      "Legal Structure": "structure",
+                      "Example identifier(s)": "example_identifiers",
+                      "Register Type": "register_type",
+                      "Available online?": "available_online",
+                      "Finding the identifiers": "guidance_on_locating_ids",
+                      "Openly licensed": "license_status",
+                      "License details": "license_details",
+                      "Online availability": "online_access_details",
+                      "Access to data": "data_access_properties",
+                      "Data access details": "data_access_details",
+                      "Data features": "dataset_features",
+                      "In OpenCorporates?": "opencorporates",
+                      "Languages supported": "languages",
+                      "Sector": "sector",
+                      "Sub-national": "subnational",
+                      "Wikipedia page": "wikipedia",
+                      "Confirmed?": "confirmed",
+                      "Last Updated": "last_updated",
+                      "Deprecated": "deprecated",
+                      "AKA": "former_prefixes",
+                      "Source": "source",
+                      "Weight": "weight"}
+
+identifier_to_name = dict((value, key) for key, value in name_to_identifier.items())
 
 
 @register.filter(name='tidy_results')
 def tidy_results(results):
     tidied_results = {}
     for key, value in results.items():
+        key_name = identifier_to_name.get(key, key)
         if key == 'structure_flat':
-            tidied_results['structure'] = ", ".join(value)
+            tidied_results[identifier_to_name['structure']] = ", ".join(value)
         if key in SKIP_KEYS:
             continue
         if value and isinstance(value, list):
             if isinstance(value[0], str):
-                tidied_results[key] = ", ".join(value)
+                tidied_results[key_name] = ", ".join(value)
                 continue
             if key == 'jurisdiction':
-                tidied_results[key] = ", ".join('{country} ({country_code})'.format(**item) for item in value)
+                tidied_results[key_name] = ", ".join('{country} ({country_code})'.format(**item) for item in value)
                 continue
             if key == 'subnational':
-                tidied_results[key] = ", ".join('{region_name} ({region_code})'.format(**item) for item in value)
+                tidied_results[key_name] = ", ".join('{region_name} ({region_code})'.format(**item) for item in value)
                 continue
-        tidied_results[key] = value
+            if key == 'sector':
+                tidied_results[key_name] = ", ".join(item['name'] for item in value)
+                continue
+        tidied_results[key_name] = value
     return sorted(tidied_results.items())
--- prefix_finder/frontend/urls.py
+++ prefix_finder/frontend/urls.py
@@ -1,7 +1,10 @@
 from django.conf.urls import url
+from django.views.generic import TemplateView
 
 from . import views
 
 urlpatterns = [
     url(r'^$', views.home, name='home'),
+    url(r'^terms', TemplateView.as_view(template_name='terms.html'), name='terms'),
+    url(r'^about', TemplateView.as_view(template_name='about.html'), name='about'),
 ]
