6b632db8713cf722fdd4e85cfec2530dbdd08370
David Raznick <kindly@gmail.com>
2017-02-15 11:56:10 +0000

    Start of change to ui to pick up new codelist and org id list structure

--- prefix_finder/frontend/views.py
+++ prefix_finder/frontend/views.py
@@ -6,53 +6,74 @@ from django.shortcuts import render
 
 
 current_dir = os.path.dirname(os.path.realpath(__file__))
-data_dir = os.path.join(current_dir, '../../data')
-
-files = ['jurisdictions', 'organisation_types', 'sectors', 'subnational']
-
-lists = {}
-
-for file in files:
-    with open(os.path.join(data_dir, file) + '.json') as data:
-        lists[file] = json.load(data)
-
-lists['jurisdictions'].sort(key=lambda k: k.get('country') or k.get('countryCode'))
-lists['organisation_types'].sort(key=lambda k: k.get('name') or '')
-
-codes_dir = os.path.join(current_dir, '../../codes')
-
-prefixes = []
-for prefix_file_name in glob.glob(codes_dir + '/*/*.json'):
-    with open(prefix_file_name) as prefix_file:
-        prefixes.append(json.load(prefix_file))
-
-lists['prefix_list'] = prefixes
 
 
+def load_files():
+    schema_dir = os.path.join(current_dir, '../../schema')
+    schemas = {}
+    for file_path in glob.glob(schema_dir + '/*.json'):
+        with open(file_path) as data:
+            schemas[file_path.split('/')[-1].split(".")[0]] = json.load(data)
 
-def flatten_structure(structure):
-    if isinstance(structure, dict):
-        for key, value in structure.items():
-            if key == 'name':
-                yield value
-            if isinstance(value, dict):
-                yield from flatten_structure(value)
+    return schemas
 
-#make_prefix_list_more_searchable by flattening some fields
-for prefix in lists['prefix_list']:
-    structure_flat = []
-    for item in prefix['structure'] or []:
-        structure_flat.extend(list(flatten_structure(item)))
-    prefix['structure_flat'] = structure_flat
-    prefix['jurisdiction_flat'] = [jurisdiction['countryCode'] for jurisdiction in prefix.get('jurisdiction') or []]
+schemas = load_files()
+
+
+def create_codelist_lookups(schemas):
+
+    lookups = {}
+
+    lookups['coverage'] = [(item['code'], item['title']['en']) for item in schemas['codelist-coverage']['coverage']] 
+    lookups['subnationalCoverage'] = [(item['code'], item['title']['en']) for item in schemas['codelist-coverage']['subnationalCoverage']] 
+
+    lookups['structure'] = [(item['code'], item['title']['en']) for item in schemas['codelist-structure']['structure'] if not item['parent']] 
+    lookups['substructure'] = [(item['code'], item['title']['en']) for item in schemas['codelist-structure']['structure'] if item['parent']] 
+
+    lookups['sector'] = [(item['code'], item['title']['en']) for item in schemas['codelist-sector']['sector']] 
+
+    return lookups
+
+lookups = create_codelist_lookups(schemas)
+
+
+def load_org_id_lists():
+    codes_dir = os.path.join(current_dir, '../../codes')
+
+    org_id_lists = []
+    for org_id_list_file in glob.glob(codes_dir + '/*/*.json'):
+        with open(org_id_list_file) as org_id_list:
+            org_id_lists.append(json.load(org_id_list))
+
+    return org_id_lists
+
+org_id_lists = load_org_id_lists()
+
+#import pprint; pprint.pprint(org_id_lists)
+
+#def flatten_structure(structure):
+    #if isinstance(structure, dict):
+        #for key, value in structure.items():
+            #if key == 'name':
+                #yield value
+            #if isinstance(value, dict):
+                #yield from flatten_structure(value)
+
+##make_prefix_list_more_searchable by flattening some fields
+#for prefix in lists['prefix_list']:
+    #structure_flat = []
+    #for item in prefix['structure'] or []:
+        #structure_flat.extend(list(flatten_structure(item)))
+    #prefix['structure_flat'] = structure_flat
+    #prefix['jurisdiction_flat'] = [jurisdiction['countryCode'] for jurisdiction in prefix.get('jurisdiction') or []]
 
 
 def filter_and_score_results(query):
-    indexed = {prefix['code']: prefix.copy() for prefix in lists['prefix_list']}
+    indexed = {org_id_list['code']: org_id_list.copy() for org_id_list in org_id_lists}
     for prefix in list(indexed.values()):
         register_type = prefix.get('registerType')
         if register_type:
-            if register_type == 'Primary':
+            if register_type == 'primary':
                 prefix['weight'] = 4
             else:
                 prefix['weight'] = 1
